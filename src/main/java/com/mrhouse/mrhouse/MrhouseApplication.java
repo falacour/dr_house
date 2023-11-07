7dda27ff348547aff57e17c81835cb151fddb914
falacour <94493464+falacour@users.noreply.github.com>
2023-11-07 20:27:22 -0300

    primer commit

---
+++ src/main/java/com/mrhouse/mrhouse/MrhouseApplication.java
@@ -0,0 +1,13 @@
+package com.mrhouse.mrhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrhouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrhouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrhouse/mrhouse/MrhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrhouse.mrhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrhouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
