a91dfa8acc2e19900596596a214af9210b17df75
Josheone <130614824+Josheone@users.noreply.github.com>
2023-11-13 11:54:35 -0300

    conflictos arreglados 1

--- src/main/java/com/mrhouse/mrhouse/controladores/PortalControlador.java
+++ src/main/java/com/mrhouse/mrhouse/controladores/PortalControlador.java
@@ -1,11 +1,15 @@
 
 package com.mrhouse.mrhouse.controladores;
 
+import com.mrhouse.mrhouse.excepciones.MiException;
 import com.mrhouse.mrhouse.servicios.ServicioUsuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping ("/")
