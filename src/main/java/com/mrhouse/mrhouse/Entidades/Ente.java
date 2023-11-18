b15702bbca896c1be1c38e5fbb61aca035df0466
nicolas <nicolasfernandez84@gmail.com>
2023-11-17 21:14:26 -0300

    no se que onda

--- src/main/java/com/mrhouse/mrhouse/Entidades/Ente.java
+++ src/main/java/com/mrhouse/mrhouse/Entidades/Ente.java
@@ -1,7 +1,6 @@
 package com.mrhouse.mrhouse.Entidades;
 
 import com.mrhouse.mrhouse.enumeraciones.Rol;
-import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -21,8 +20,10 @@ public class Ente {
     private String nombre;
     private String email;
     private String password;
-    private List<Inmueble> inmueble;
-    
+
+   
+//    private List<Inmueble> inmueble;
+
 @ManyToOne
 private Imagen imagen;
     @Enumerated(EnumType.STRING)
@@ -31,13 +32,13 @@ private Imagen imagen;
     public Ente() {
     }
 
-    public List<Inmueble> getInmueble() {
-        return inmueble;
-    }
-
-    public void setInmueble(List<Inmueble> inmueble) {
-        this.inmueble = inmueble;
-    }
+//    public List<Inmueble> getInmueble() {
+//        return inmueble;
+//    }
+//
+//    public void setInmueble(List<Inmueble> inmueble) {
+//        this.inmueble = inmueble;
+//    }
 
 
     public String getId() {
