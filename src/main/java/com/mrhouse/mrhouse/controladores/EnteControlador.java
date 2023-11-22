f7ce625a06032a625248ea310958caacb763c71e
falacour <94493464+falacour@users.noreply.github.com>
2023-11-22 19:16:59 -0300

    Se agrega funcionalidad de la vista ente
    Se esta trabajando en la vista de modificar

--- src/main/java/com/mrhouse/mrhouse/controladores/EnteControlador.java
+++ src/main/java/com/mrhouse/mrhouse/controladores/EnteControlador.java
@@ -7,6 +7,7 @@ package com.mrhouse.mrhouse.controladores;
 import com.mrhouse.mrhouse.Entidades.Ente;
 import com.mrhouse.mrhouse.excepciones.MiException;
 import com.mrhouse.mrhouse.servicios.ServicioEnte;
+import com.mrhouse.mrhouse.servicios.ServicioInmueble;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
