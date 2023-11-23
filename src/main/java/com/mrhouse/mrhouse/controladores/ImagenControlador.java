36be1439554415c68d8841337e3f53e6ae0a6457
Josheone <130614824+Josheone@users.noreply.github.com>
2023-11-23 20:09:32 -0300

    error de imagenes arreglado

--- src/main/java/com/mrhouse/mrhouse/controladores/ImagenControlador.java
+++ src/main/java/com/mrhouse/mrhouse/controladores/ImagenControlador.java
@@ -1,7 +1,8 @@
+package com.mrhouse.mrhouse.controladores;
 
-import com.mrhouse.mrhouse.Entidades.Inmueble;
+import com.mrhouse.mrhouse.Entidades.Imagen;
+import com.mrhouse.mrhouse.servicios.ServicioImagen;
 import com.mrhouse.mrhouse.servicios.ServicioInmueble;
-import java.util.List;
 import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,7 +10,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,18 +20,24 @@ public class ImagenControlador {
 
     @Autowired
     ServicioInmueble servicioInmueble;
+    @Autowired
+    ServicioImagen servicioImagen;
 
     @GetMapping("/inmueble/{id}")
-    public ResponseEntity<byte[]> imagenUsuario(@PathVariable Long id) {
-    try {
-        Inmueble inmueble = servicioInmueble.getOne(id);
-        byte[] imagen = inmueble.getImagen().getContenido();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.IMAGE_JPEG);
-        return new ResponseEntity<>(imagen, headers, HttpStatus.OK);
-    } catch (EntityNotFoundException e) {
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public ResponseEntity<byte[]> imagenUsuario(@PathVariable String id) {
+        try {
+            Imagen img = servicioImagen.getOne(id);
+
+            byte[] imagen = img.getContenido();
+
+            HttpHeaders headers = new HttpHeaders();
+
+            headers.setContentType(MediaType.IMAGE_JPEG);
+
+            return new ResponseEntity<>(imagen, headers, HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
-}
 
 }
